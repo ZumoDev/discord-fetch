26ce86c019e22cde603b0752c032c10a37fd0120
ZumoDev <72050616+ZumoDev@users.noreply.github.com>
2021-10-13 18:34:16 -0500

    Transfer req.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./req.js')
+const fetch = require('node-fetch')
+
+module.exports = async (path, obj) => {
+    if (!path) throw new Error('No path was provided')
+    if (!obj || typeof obj != 'object') throw new SyntaxError('"obj" must be an object')
+
+    let r = await fetch('https://discord.com/api/v9'+path, obj)
+    return await r.json()
+
+}
